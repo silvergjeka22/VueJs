544aa5d02391a9d083d5abc4a9282ab5f5b69153
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2023-04-22 16:58:54 +0200

    tailwind

--- vue-search/src/App.vue
+++ vue-search/src/App.vue
@@ -1,10 +1,3 @@
-<script setup>
-import HelloWorld from '@/components/HelloWorld.vue' 
-</script>
-
 <template>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <HelloWorld msg="Nice Job!" ><!--You can put somthing--></HelloWorld>
-    </div>
+      <h1>Hello VueJs</h1>
 </template>
